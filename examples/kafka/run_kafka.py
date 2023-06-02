0c956dcb680270f8e2e04f4e7912692dd60650fe
tesfa <tesfaaog@gmail.com>
2023-06-02 12:50:43 -0700

    Upgrading bytewax

--- examples/kafka/run_kafka.py
+++ examples/kafka/run_kafka.py
@@ -1,8 +1,8 @@
 import os
-import pointstorm.ingestion.event.kafka as kafka
+from pointstorm.ingestion.event.kafka import KafkaTextEmbeddings
 
 if __name__ == "__main__":
-    kafka_embeddings = kafka.KafkaTextEmbeddings(
+    kafka_embeddings = KafkaTextEmbeddings(
         kafka_topic="user-tracker",
         kafka_bootstrap_server="localhost:9092",
         kafka_group_id=f"kafka_text_vectorizer_{os.getpid()}",
